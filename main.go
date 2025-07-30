e62eda127210c46d772532ea890370867b0deaf1
Rlek <wirat0khamphan@gmail.com>
2025-07-30 13:15:15 +0700

    cors

--- main.go
+++ main.go
@@ -10,6 +10,20 @@ func main() {
 
 	app := fiber.New()
 
+	// Setup CORS middleware
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", "*")
+		c.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Method() == "OPTIONS" {
+			c.SendStatus(fiber.StatusNoContent)
+			return nil
+		}
+
+		return c.Next()
+	})
 	routes.RegisterRoutes(app)
 
 	app.Listen(":8080")
