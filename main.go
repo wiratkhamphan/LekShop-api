3079b34d070f54d755589bbbb64ac391b0105921
Rlek <wirat0khamphan@gmail.com>
2025-09-02 16:33:17 +0700

    *

--- main.go
+++ main.go
@@ -14,14 +14,25 @@ import (
 func main() {
 	app := fiber.New()
 
-	allow := os.Getenv("ALLOW_ORIGINS")
-	if strings.TrimSpace(allow) == "" {
-		// ค่า default สำหรับ dev
-		allow = "*"
+	origins := os.Getenv("ALLOW_ORIGINS")
+	var allowList []string
+
+	if strings.TrimSpace(origins) == "" {
+		// default dev
+		allowList = []string{"http://localhost:3000", "http://127.0.0.1:5500", "https://lek-shop.vercel.app"}
+	} else {
+		allowList = strings.Split(origins, ",")
 	}
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     allow, 
+		AllowOriginsFunc: func(origin string) bool {
+			for _, o := range allowList {
+				if strings.TrimSpace(o) == origin {
+					return true
+				}
+			}
+			return false
+		},
 		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Set-Cookie",
